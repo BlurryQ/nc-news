d84c333cfb11643a5df1956db04f0f0cd3efe9e3
BlurryQ <jazz.barlow@outlook.com>
2024-09-19 09:22:38 +0100

    Completed Task 6

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import "../styles/article.css"
 import { useEffect, useState } from 'react'
-import { RingLoader } from "react-spinners"
+import { RingLoader, GridLoader } from "react-spinners"
 import { useParams } from "react-router-dom"
 import { format } from "date-fns";
 import getArticles from "../APIs/getArticleAPI"
@@ -8,8 +8,11 @@ import getArticles from "../APIs/getArticleAPI"
 
 export default function Article() {
     const [article, setArticle] = useState([])
+    const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [hasError, setHasError] = useState(false)
+    const [isLoadingComments, setIsLoadingComments] = useState(true)
+    const [hasCommentsErrored, setHasCommentsErrored] = useState(false)
 
     const {article_id} = useParams()
 
@@ -29,6 +32,22 @@ export default function Article() {
         })
     }, [])
 
+    useEffect(() => {
+        setIsLoadingComments(true)
+        setHasCommentsErrored(false)
+        getArticles(`${article_id}/comments`)
+        .then((data) => {
+            setIsLoadingComments(false)
+            setHasCommentsErrored(false)
+            setComments(data.comments)
+        })
+        .catch(err => {
+            console.error(err);
+            setIsLoadingComments(false)
+            setHasCommentsErrored(true)
+        })
+    },[])
+
     if(isLoading) return <RingLoader
         color="red"
         size={320}
@@ -38,7 +57,7 @@ export default function Article() {
 
     if(hasError) return <section className="page-not-found"></section>
 
-    return <div className="article-container">
+return <div className="article-container">
         <h2 className="page-title">{article.title}</h2>
         <img className="article-img" src={article.article_img_url} alt={article.title} />
         <div className="article-details">
@@ -52,5 +71,30 @@ export default function Article() {
                 <button className="dislike-button"></button>
             </div>
         <section>{article.body}</section>
+
+        {hasCommentsErrored ? <p>Error loading comments...</p> : null}
+
+        {isLoadingComments ? <GridLoader
+            color="red"
+            size={20}
+            margin={20}
+            cssOverride={{margin: "auto"}}
+            aria-label="Loading Spinner"/> : null}
+
+        <section id="comments" className="comments-section">
+            {comments.map(comment=>{
+                return <div key={`${comment.comment_id}`} className="comment-container">
+                    <p className="comment-body">"{comment.body}"</p>
+                    <div className="comment-details">
+                        <p className="article-author">~ {comment.author}</p>
+                        <button className="like-button">{comment.votes >= 0 
+                                    ? comment.votes 
+                                    : comment.votes}</button>
+                        <button className="dislike-button"></button>
+                        <p>{format((comment.created_at), "HH:mm, dd MMM yy")}</p>
+                    </div>
+                </div>
+            })}
+        </section>
     </div>
 }
