d459f566a412feaed255b192b1befb7c4235af68
BlurryQ <jazz.barlow@outlook.com>
2024-11-05 13:03:58 +0000

    Correct function name

--- src/APIs/deleteComment.js
+++ src/APIs/deleteComment.js
@@ -4,7 +4,7 @@ const articlesAPI = axios.create({
   baseURL: "https://nc-news-lbn1.onrender.com/api/comments",
 });
 
-export default function deleteArticles(commentID) {
+export default function deleteComment(commentID) {
   return articlesAPI.delete(`/${commentID}`).then((response) => {
     return response.status;
   });
