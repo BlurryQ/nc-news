61bd4d154636b856d9a72def2683360df976a11f
BlurryQ <jazz.barlow@outlook.com>
2024-11-05 13:05:47 +0000

    Prop drill parameter

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -17,37 +17,37 @@ export default function Article() {
     const [articleVoteError, setArticleVoteError] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
     const [hasError, setHasError] = useState(false)
-    const {user} = useContext(UserContext)
-    const {article_id} = useParams()
+    const { user } = useContext(UserContext)
+    const { article_id } = useParams()
 
     useEffect(() => {
         setIsLoading(true)
         setHasError(false)
         getArticles(article_id)
-        .then((data)=>{
-            setIsLoading(false)
-            setHasError(false)
-            setArticle(data.article)
-            setArticleVotes(data.article.votes)
-        })
-        .catch(err => {
-            console.error(err);
-            setIsLoading(false)
-            setHasError(true)
-        })
+            .then((data) => {
+                setIsLoading(false)
+                setHasError(false)
+                setArticle(data.article)
+                setArticleVotes(data.article.votes)
+            })
+            .catch(err => {
+                console.error(err);
+                setIsLoading(false)
+                setHasError(true)
+            })
     }, [])
 
-    if(isLoading) return <RingLoader
+    if (isLoading) return <RingLoader
         color="red"
         size={320}
         aria-label="Loading Spinner"
-        cssOverride={{margin: "auto"}}
+        cssOverride={{ margin: "auto" }}
     />
 
-    if(hasError) return <section className="page-not-found"></section>
+    if (hasError) return <section className="page-not-found"></section>
 
     const toggleVoteClass = (button, buttonPressed, alreadyVoted) => {
-        if(alreadyVoted) {
+        if (alreadyVoted) {
             button.classList.remove("voted")
             setArticleVotedOn(false)
         }
@@ -59,7 +59,7 @@ export default function Article() {
 
     const likeHandler = (e) => {
         setArticleVoteError(false)
-        if(!user.username) {
+        if (!user.username) {
             setArticleVoteError("Please sign in to vote")
             return
         }
@@ -69,19 +69,19 @@ export default function Article() {
         toggleVoteClass(button, buttonPressed, alreadyVoted)
         const adjust = voteAdjustment(buttonPressed, alreadyVoted, articleVotedOn)
         setArticleVotes(articleVotes + adjust)
-        patchArticles(`${article_id}`,{ inc_votes: adjust })
-        .then(() => {
-            setArticleVoteError(false)
-        })
-        .catch(err => {
-            toggleVoteClass(button, buttonPressed, alreadyVoted)
-            const voteButtons = document.getElementsByName("vote")
-            voteButtons.forEach(button => button.classList.remove("voted"))
-            console.error(err);
-            setArticleVoteError("Error has occured, please try again later")
-            setArticleVotes(articleVotes)
+        patchArticles(`${article_id}`, { inc_votes: adjust })
+            .then(() => {
+                setArticleVoteError(false)
+            })
+            .catch(err => {
+                toggleVoteClass(button, buttonPressed, alreadyVoted)
+                const voteButtons = document.getElementsByName("vote")
+                voteButtons.forEach(button => button.classList.remove("voted"))
+                console.error(err);
+                setArticleVoteError("Error has occured, please try again later")
+                setArticleVotes(articleVotes)
 
-        })
+            })
     }
 
 
@@ -93,14 +93,14 @@ export default function Article() {
             <p>{format((article.created_at), "EEEE do MMMM yyyy")}</p>
             <p className="article-author">~ {article.author}</p>
         </div>
-            <div className="likes-comments">
-                <button onClick={likeHandler} name="vote" id="like-button" className="like-button">{articleVotes}</button>
-                <a href="#comments" className="comment-tally">{article.comment_count}</a>
-                <button onClick={likeHandler} name="vote" id="dislike-button" className="dislike-button"></button>
-            </div>
-            {articleVoteError ? <p className="article-vote-error">{articleVoteError}</p> : null}
+        <div className="likes-comments">
+            <button onClick={likeHandler} name="vote" id="like-button" className="like-button">{articleVotes}</button>
+            <a href="#comments" className="comment-tally">{article.comment_count}</a>
+            <button onClick={likeHandler} name="vote" id="dislike-button" className="dislike-button"></button>
+        </div>
+        {articleVoteError ? <p className="article-vote-error">{articleVoteError}</p> : null}
         <section>{article.body}</section>
 
-        <Comments />
+        <Comments article_id={article_id} />
     </div>
 }
